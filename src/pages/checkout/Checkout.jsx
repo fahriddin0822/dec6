d98bd3888a4c35c278e2b2bdf2e4d6a451b22688
Fahriddin <mavlonovfahriddin0822@gmail.com>
2024-12-07 08:16:22 +0500

    app is made

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -2,41 +2,54 @@ import React, { useRef } from "react";
 import { useCartContext } from "../../context/carts";
 import { Navigate } from "react-router-dom";
 
-const userID = 6222544109
-const BOT_TOKEN="7789079010:AAH3PMOEu3CB2UjixrwwdZrNMrZFca7urzQ"
-const chatId  = -1002422290055
-
-// https://api.telegram.org/bot7789079010:AAH3PMOEu3CB2UjixrwwdZrNMrZFca7urzQ/getUpdates
-// https://api.telegram.org/bot7789079010:AAH3PMOEu3CB2UjixrwwdZrNMrZFca7urzQ/sendMessage?chat_id=[your chat_id]
-
+const chatId = -1002422290055;
+const BOT_TOKEN = "7789079010:AAH3PMOEu3CB2UjixrwwdZrNMrZFca7urzQ";
 
 const Checkout = () => {
   const { cartlist } = useCartContext();
 
-  if(!cartlist.length){
-    return <Navigate replace to={"/carts"}/>
+  if (!cartlist.length) {
+    return <Navigate replace to={"/carts"} />;
   }
 
   const fname = useRef(null);
   const lname = useRef(null);
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(fname.current.value);
-    console.log(lname.current.value);
-    let url = `https://api.telegram.org/bot7789079010:AAH3PMOEu3CB2UjixrwwdZrNMrZFca7urzQ/sendMessage?chat_id=${chatId}&text=${fname.current.value}-${lname.current.value}`
-    let api = new XMLHttpRequest()
 
-    api.open("GET", url, true)
-    api.send("")
+    const firstName = fname.current.value;
+    const lastName = lname.current.value;
+
+    // Construct a message with product details
+    const productsDetails = cartlist
+      .map(
+        (product) =>
+          `${product.title} (Amount: ${product.amount}, Price: ${product.price}$)`
+      )
+      .join("\n");
+
+    const message = `Order Details:
+Name: ${firstName} ${lastName}
+Products:
+${productsDetails}`;
+
+    const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(
+      message
+    )}`;
+
+    // Send message to Telegram
+    let api = new XMLHttpRequest();
+    api.open("GET", url, true);
+    api.send();
   };
 
   return (
     <div className="min-h-[80vh] w-full bg-white">
       <h2>Checkout</h2>
       <form onSubmit={handleSubmit}>
-        <input ref={fname} type="text" className="border" />
-        <input ref={lname} type="text" className="border" />
+        <input ref={fname} type="text" placeholder="First Name" className="border" />
+        <input ref={lname} type="text" placeholder="Last Name" className="border" />
         <button>Submit</button>
       </form>
     </div>
